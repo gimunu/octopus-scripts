0e8f0f66c6de8d9c8bdfce5529bffd591f06e8b1
umberto <umberto.degiovannini@gmail.com>
2016-06-28 13:26:32 +0200

    track progress

--- bands2h.py
+++ bands2h.py
@@ -21,6 +21,7 @@ from __future__ import division
 
 import argparse
 import sys
+import time
 
 try: 
     import numpy as np
@@ -38,6 +39,31 @@ import os
 
 import warnings
 # warnings.simplefilter('error', UserWarning)
+
+
+# update_progress() : Displays or updates a console progress bar
+## Accepts a float between 0 and 1. Any int will be converted to a float.
+## A value under 0 represents a 'halt'.
+## A value at 1 or bigger represents 100%
+def update_progress(progress):
+    barLength = 10 # Modify this to change the length of the progress bar
+    status = ""
+    if isinstance(progress, int):
+        progress = float(progress)
+    if not isinstance(progress, float):
+        progress = 0
+        status = "error: progress var must be float\r\n"
+    if progress < 0:
+        progress = 0
+        status = "Halt...\r\n"
+    if progress >= 1:
+        progress = 1
+        status = "Done...\r\n"
+    block = int(round(barLength*progress))
+    text = "\rPercent: [{0}] {1}% {2}".format( "#"*block + "-"*(barLength-block), progress*100, status)
+    sys.stdout.write(text)
+    sys.stdout.flush()
+
 
 def get_prj_root_from_file(file):
     path = os.path.dirname(os.path.abspath(file))
@@ -415,6 +441,9 @@ def get_Ei_from_bands (kx,ky,kz,kmesh, nbands, bands, dim, other = None ):
                 # according only to energy ordering
                 E[i,:,:] = E[i, np.argsort(E[i,:,0])]
 
+        update_progress(i/(kx.shape[0]-1))
+    
+    print ""
     return E
     
 
